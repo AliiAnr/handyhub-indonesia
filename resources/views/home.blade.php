6546da1a6363b648fc54278733ec2dc4ea10f6fb
AliiAnr <alianur043@gmail.com>
2023-05-17 09:54:19 +0800

    projects

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Test Website</title>
+</head>
+<body>
+   <h1>YEEEY</h1>
+   <p> <?php echo $Name; ?></p>
+   <p> <?php echo $Email; ?></p>
+   <img src="<?php echo $image ?>" alt="<?php $name ?>">
+</body>
+</html>
